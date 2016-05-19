1b7f6e67e1dbd7c2ef0be7dd3659c468382aaed6
Joey McDonald <joey@ubuntu-maas.cloud-band.com>
2016-05-19 14:33:58 -0700

    Updating MAAS trusty install.

---
+++ maas/maas-trusty-install.sh
@@ -0,0 +1,34 @@
+# MAAS Trusty Install
+# Execute as sudo ./maas-trusty-install.sh
+
+function install_maas() {
+
+   apt-get -y install software-properties-common vim
+   apt-add-repository -y ppa:maas/stable
+   apt-get update
+   apt-get -y install maas
+}
+
+function configure_maas() {
+   ip=$(ifconfig eth0|perl -lane 'print $1 if (/t addr:(.*?)\s/)')
+
+   # Create the MAAS login session
+   sudo maas-region-admin createadmin --username=admin --password=admin --email=nobody@cloud-band.com
+   maas login admin http://$ip/MAAS/api/1.0 $(sudo maas-region-admin apikey --username=admin)
+
+   uuid=$(maas admin node-groups list | grep uuid | cut -d\" -f4)
+
+   maas admin node-group-interface update $uuid eth0 \
+        ip_range_high=10.1.0.200    \
+        ip_range_low=10.1.0.100     \
+        management=2                     \
+        broadcast_ip=10.1.0.255     \
+        router_ip=10.1.0.1          \
+}
+
+function import_images() {
+
+   # Import some images
+   # maas admin boot-source-selections create 2 os="ubuntu" release="xenial" arches="amd64" subarches="*" labels="*"
+   maas admin boot-resources import
+}
