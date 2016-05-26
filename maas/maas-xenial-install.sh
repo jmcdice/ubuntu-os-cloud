b7520ddeaab9bc0c2bdc365fa6de2765854fa6cb
Joey McDonald <joey@ubuntu-maas.cloud-band.com>
2016-05-25 20:03:56 -0700

    I don't even remember what changed.

--- maas/maas-xenial-install.sh
+++ maas/maas-xenial-install.sh
@@ -6,45 +6,49 @@ function install_maas() {
 
    pkgs='git vim lsof tcpdump strace htop nload software-properties-common'
    sudo apt-get -y install $pkgs
-   sudo apt-add-repository ppa:maas/next
-   sudo apt-get update
+   # sudo apt-add-repository ppa:maas/next
+   # sudo apt-get update
    sudo apt-get -y install maas
 }
 
 function configure_maas() {
 
-   ip=$(ifconfig eth0|perl -lane 'print $1 if (/t addr:(.*?)\s/)')
+   iface=$(route -n |grep ^0.0.0.0| awk '{print $NF}')
+   ip=$(ifconfig $iface|perl -lane 'print $1 if (/t addr:(.*?)\s/)')
 
    # Create the MAAS login session
-   sudo maas-region-admin createadmin --username=admin --password=admin --email=nobody@cloud-band.com
-   maas login admin http://$ip/MAAS/api/2.0 $(sudo maas-region-admin apikey --username=admin)
+   sudo maas-region createadmin --username=admin --password=admin --email=nobody@cloud-band.com
+   sleep 3
+   maas login admin http://$ip/MAAS/api/2.0 $(sudo maas-region apikey --username=admin)
 }
 
 function configure_private() {
 
    uuid=$(maas admin node-groups list | grep uuid | cut -d\" -f4)
+   iface=$(route -n |grep ^0.0.0.0| awk '{print $NF}')
 
-   maas admin node-group-interface update $uuid eth0 \
+   echo "UUID: $uuid"
+   echo "Iface: $iface"
+
+   echo "maas admin node-group-interface update -d $uuid $iface \
       ip_range_low=10.1.0.100 \
       ip_range_high=10.1.0.200 \
       static_ip_range_low=10.1.0.50 \
       static_ip_range_high=10.1.0.99 \
       management=2  \
       broadcast_ip=10.1.0.255 \
-      router_ip=10.1.0.1
+      router_ip=10.1.0.1"
 
 }
 
 function import_images() {
 
-   # Import some images
-   # maas admin boot-source-selections create 2 os="ubuntu" release="xenial" arches="amd64" subarches="*" labels="*"
-   maas admin boot-resources import
+   # Import Xenial
+   maas admin boot-source-selection create 1 os="ubuntu" release="trusty" arches="amd64" subarches="*" labels="*"
 }
 
 install_maas
-exit
 configure_maas
 sleep 60 
-configure_private
-import_images
+#configure_private
+#import_images
--- vm-builder/03-maas-xenial.sh
+++ vm-builder/03-maas-xenial.sh
@@ -7,20 +7,33 @@ source ./vm-builder-common.sh || ( echo "Failed: Need vm-builder-common.sh" && e
 # Env specific
 vm='maas-xenial'
 rel='xenial'
-ip='10.1.0.22'
+ip='10.1.0.23'
 mask='255.255.255.0'
 net='10.1.0.0'
 bcast='10.1.0.255'
 gw='10.1.0.1'
+edns='135.1.1.111'
 
 domain='cloud-band.com'
 user='joey'
 pass='password01'
 
+sudo umount /mnt/
+start_vm
+
+exit
 echo "" > ~/.ssh/known_hosts
 #build_vm
-#sudo guestmount -a maas-xenial/tmpELDFKn.qcow2 -i /mnt/
+#exit
+qcow=$(dir -1 maas-xenial/*qcow2)
+echo "Operating on: $qcow"
+sudo guestmount -a $qcow -i /mnt/
 sudo sed -i 's/eth0/ens3/' /mnt/etc/network/interfaces
+sudo mkdir -p /mnt/home/$user/.ssh/
+sudo cp ~/.ssh/id_rsa.pub /mnt/home/$user/.ssh/authorized_keys
+sudo chown -R $user:$user /mnt/home/$user/.ssh/
+sudo chmod 600 /mnt/home/$user/.ssh/authorized_keys
+sudo sh -c "echo 'nameserver $edns' >> /mnt/etc/resolv.conf"
 sudo umount /mnt/
 start_vm
 
