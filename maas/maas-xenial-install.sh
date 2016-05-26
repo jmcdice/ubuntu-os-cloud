2172e604e52d85de80f202708b2567cc6cb0f1d7
Joey McDonald <joey@ubuntu-maas.cloud-band.com>
2016-05-25 21:27:25 -0700

    I actually have it working. Amazing.

--- maas/maas-xenial-install.sh
+++ maas/maas-xenial-install.sh
@@ -1,13 +1,13 @@
 # MAAS Xenial Instalal
 # 
-# bash maas-trusty-install.sh
+# bash maas-xenial-install.sh
 
 function install_maas() {
 
    pkgs='git vim lsof tcpdump strace htop nload software-properties-common'
    sudo apt-get -y install $pkgs
-   # sudo apt-add-repository ppa:maas/next
-   # sudo apt-get update
+   sudo apt-add-repository ppa:maas/next
+   sudo apt-get update
    sudo apt-get -y install maas
 }
 
@@ -22,33 +22,20 @@ function configure_maas() {
    maas login admin http://$ip/MAAS/api/2.0 $(sudo maas-region apikey --username=admin)
 }
 
-function configure_private() {
+function configure_dhcp() {
 
-   uuid=$(maas admin node-groups list | grep uuid | cut -d\" -f4)
-   iface=$(route -n |grep ^0.0.0.0| awk '{print $NF}')
-
-   echo "UUID: $uuid"
-   echo "Iface: $iface"
-
-   echo "maas admin node-group-interface update -d $uuid $iface \
-      ip_range_low=10.1.0.100 \
-      ip_range_high=10.1.0.200 \
-      static_ip_range_low=10.1.0.50 \
-      static_ip_range_high=10.1.0.99 \
-      management=2  \
-      broadcast_ip=10.1.0.255 \
-      router_ip=10.1.0.1"
-
+   hostname=$(hostname)
+   maas admin ipranges create type=dynamic start_ip=10.1.0.28 end_ip=10.1.0.100
+   maas admin vlan update fabric-0 untagged dhcp_on=True primary_rack="$hostname"
 }
 
 function import_images() {
 
-   # Import Xenial
-   maas admin boot-source-selection create 1 os="ubuntu" release="trusty" arches="amd64" subarches="*" labels="*"
+   maas admin boot-resources import
 }
 
 install_maas
 configure_maas
 sleep 60 
-#configure_private
-#import_images
+import_images
+configure_dhcp
